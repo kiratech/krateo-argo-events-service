62d8fd04f36a4e4216775fadf3cd2b70e397886e
mauro sala <mauro.sala@gmail.com>
2022-05-18 15:33:24 +0200

    feat(controllers): trigger and project

--- controllers/create.keptn.controller.js
+++ controllers/create.keptn.controller.js
@@ -5,7 +5,7 @@ const uriHelpers = require('../helpers/uri.helpers')
 const stringHelpers = require('../helpers/string.helpers')
 const axios = require('axios')
 
-router.get('/project/:endpoint/:name', async (req, res, next) => {
+router.post('/trigger/:endpoint/:name', async (req, res, next) => {
   try {
     const endpoint = JSON.parse(stringHelpers.b64toAscii(req.params.endpoint))
     const name = stringHelpers.b64toAscii(req.params.name)
@@ -15,32 +15,17 @@ router.get('/project/:endpoint/:name', async (req, res, next) => {
     const token = endpoint.secret.find((x) => x.key === 'token')
 
     const regex = /(?<=\[)[^\]\[]*(?=])/gm
-    const projectName = name.match(regex)[0]
 
-    const prj = await axios.get(
-      uriHelpers.concatUrl([
-        endpoint.target,
-        'api/controlPlane/v1/project',
-        projectName
-      ]),
+    await axios.post(
+      uriHelpers.concatUrl([endpoint.target, 'api/v1/event']),
+      req.body,
       {
         headers: {
           'x-token': token.val
         }
       }
     )
-
-    const serviceName = name.replace(`[${projectName}]`, '')
-
-    res.status(200).json({
-      ...prj.data,
-      stages: prj.data.stages.map((x) => {
-        return {
-          ...x,
-          services: x.services.filter((x) => x.serviceName === serviceName)
-        }
-      })
-    })
+    res.status(200).json({})
   } catch (error) {
     next(error)
   }
--- routes/keptn.routes.js
+++ routes/keptn.routes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const readController = require('../controllers/read.keptn.controller')
+const createController = require('../controllers/create.keptn.controller')
 
 router.use('/', readController)
+router.use('/', createController)
 
 module.exports = router
